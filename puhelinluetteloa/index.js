0d7ce014fe7079f50550c37fd05d9366590856ba
Minttu Paalanen <minttupaalanen@gmail.com>
2025-07-04 16:09:04 +0300

    3.1-3.6 (not done)

---
+++ puhelinluetteloa/index.js
@@ -0,0 +1,102 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+let persons = [
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/info", (request, response) => {
+  response.send(`
+    <html>
+      <body>
+        <p>The phonebook has info for ${persons.length} people.</p>
+        <p>${Date()}</p>
+      </body>
+    </html>
+  `);
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((note) => note.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((person) => person.id !== id);
+  response.status(204).end();
+});
+
+const generateId = () => {
+  const maxId =
+    persons.length > 0 ? Math.max(...persons.map((n) => Number(n.id))) : 0;
+  return String(maxId + 1);
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  console.log(body);
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  console.log("test");
+
+  console.log(persons.length);
+  const alreadyExists = persons.includes((person) => {
+    console.log("tets");
+    console.log(person.name, body.name);
+    return person.name === body.name || person.number === body.number;
+  });
+
+  console.log(alreadyExists);
+  if (alreadyExists) {
+    return response.status(400).json({
+      error: "content already exists",
+    });
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+  persons = persons.concat(person);
+  response.json(person);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
