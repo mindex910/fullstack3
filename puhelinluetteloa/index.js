6b9f8a8fa9206ec08a5a6a943fdbcba50843d7c6
Minttu Paalanen <minttupaalanen@gmail.com>
2025-07-09 12:13:04 +0300

    3.9 started

--- puhelinluetteloa/index.js
+++ puhelinluetteloa/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors")
 const app = express();
 app.use(express.json());
 app.use(morgan('tiny'))
+app.use(cors())
 
 let persons = [
   {
