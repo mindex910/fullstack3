e81f22375fa803585f49db26119d1f46f2dd8b30
Minttu Paalanen <minttupaalanen@gmail.com>
2025-07-29 22:16:43 +0300

    3.13 pt2

--- puhelinluetteloa/index.js
+++ puhelinluetteloa/index.js
@@ -1,36 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
-const cors = require("cors")
+const cors = require("cors");
 const app = express();
-const Person = require('./models/persons')
-require('dotenv').config()
+const Person = require("./models/persons");
+require("dotenv").config();
 app.use(express.json());
-app.use(morgan('tiny'))
-app.use(cors())
-app.use(express.static('dist'))
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.static("dist"));
 
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
 
 app.get("/info", (request, response) => {
   response.send(`
@@ -44,19 +22,18 @@ app.get("/info", (request, response) => {
 });
 
 app.get("/api/persons", (request, response) => {
-  Person.find({}).then(persons => {
-    response.json(persons)
-  })
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get("/api/persons/:id", (request, response) => {
   const id = request.params.id;
-  const person = persons.find((note) => note.id === id);
-  if (person) {
+  Person.findOne({_id: id})
+  .then((person) => {
     response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  })
+  .catch(()=>{response.status(404).end()})
 });
 
 app.delete("/api/persons/:id", (request, response) => {
